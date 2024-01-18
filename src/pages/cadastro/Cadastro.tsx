238173405e577b8800f8aee29de837bbc23a6043
Kaellen-heleni <KAELLENHELENI@ICLOUD.COM>
2024-01-18 13:16:48 -0300

    Corrigido link do deploy

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Usuario from '../../models/Usuario'
+import Usuario from '../../model/Usuario'
 import { cadastrarUsuario } from '../../services/Service'
 import './Cadastro.css'
 
